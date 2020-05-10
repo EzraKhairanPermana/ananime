ec2287488ad306b30b48eac4dc12c9609e60d16e
EzraKhairanPermana <ezrakhairan123@gmail.com>
2020-05-10 12:08:29 +0700

    Ultilitas | Ditambahkan | Image.js

---
+++ src/App/Util/Image.js
@@ -0,0 +1,5 @@
+import wpuImg from "../../img/wpu.png";
+import kunoleps from "../../img/kunoleps.png";
+import ArrowRight from "../../img/arrow-right.svg";
+
+export { wpuImg, kunoleps, ArrowRight };
