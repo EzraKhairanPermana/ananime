080618f1382ffecafe7a79736b045f1754d5fbe9
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-04-11 09:49:13 +0700

    Menambahkan penggunaan @loadable/component

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,11 @@
+import loadable from "@loadable/component";
 import Provider from "./Context/Provider";
-import { Left, Middle, Right, ModalMessage, ModalDetail } from "./Components";
+import Left from "./Components/Left";
+
+const Middle = loadable(() => import("./Components/Middle"));
+const Right = loadable(() => import("./Components/Right"));
+const ModalMessage = loadable(() => import("./Components/ModalMessage"));
+const ModalDetail = loadable(() => import("./Components/ModalDetail"));
 
 const App = () => (
   <Provider>
