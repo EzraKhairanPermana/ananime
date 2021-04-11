274982d5c210d12340e246950e846c87cc821a13
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-04-11 14:41:53 +0700

    Menghapus index.js components dan menambahkan fallback

--- src/App/Components/index.js
+++
@@ -1,7 +0,0 @@
-import Left from "./Left";
-import Middle from "./Middle";
-import Right from "./Right";
-import ModalMessage from "./ModalMessage";
-import ModalDetail from "./ModalDetail";
-
-export { Left, Middle, Right, ModalMessage, ModalDetail };
--- src/App/index.js
+++ src/App/index.js
@@ -2,8 +2,16 @@ import loadable from "@loadable/component";
 import Provider from "./Context/Provider";
 import Left from "./Components/Left";
 
-const Middle = loadable(() => import("./Components/Middle"));
-const Right = loadable(() => import("./Components/Right"));
+const Middle = loadable(() => import("./Components/Middle"), {
+  fallback: (
+    <section className="web-middle">
+      <h2 id="title-season">Anime Season {new Date().getFullYear()}</h2>
+    </section>
+  ),
+});
+const Right = loadable(() => import("./Components/Right"), {
+  fallback: <section className="web-right" />,
+});
 const ModalMessage = loadable(() => import("./Components/ModalMessage"));
 const ModalDetail = loadable(() => import("./Components/ModalDetail"));
 
