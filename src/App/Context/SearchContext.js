6009ae02a1b2fd4be6db12a5ff70998f5124d48a
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-04-11 15:35:54 +0700

    Menambahkan kondisi jika search value string kosong

--- src/App/Context/SearchContext.js
+++ src/App/Context/SearchContext.js
@@ -6,31 +6,42 @@ import {
   useMemo,
 } from "react";
 import { DataContext } from "./DataContext";
+import { CurrentContext } from "./CurrentContext";
 import Axios from "axios";
 
 export const SearchContext = createContext("");
 
 export default function SearchProvider(props) {
+  const {
+    current: { setCurrent },
+  } = useContext(CurrentContext);
   const { setLoading, setData } = useContext(DataContext);
   const [searchValue, setSearchValue] = useState("");
 
   const fireSearch = useCallback(
     async (e) => {
       if (e.key === "Enter") {
-        setSearchValue("");
-        setLoading(true);
-        const data = await Axios.get("https://api.jikan.moe/v3/search/anime", {
-          params: {
-            q: searchValue,
-            limit: 32,
-          },
-        }).then((res) => res.data.results);
+        if (searchValue !== "") {
+          setSearchValue("");
+          setLoading(true);
+          const data = await Axios.get(
+            "https://api.jikan.moe/v3/search/anime",
+            {
+              params: {
+                q: searchValue,
+                limit: 32,
+              },
+            }
+          ).then((res) => res.data.results);
 
-        setData(data);
-        setLoading(false);
+          setData(data);
+          setLoading(false);
+        } else {
+          setCurrent("");
+        }
       }
     },
-    [searchValue, setData, setLoading]
+    [searchValue, setData, setLoading, setCurrent]
   );
 
   const providerValue = useMemo(
