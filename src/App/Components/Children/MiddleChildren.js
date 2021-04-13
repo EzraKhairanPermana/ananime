6bb45bf6991a5f84a60d1be58077a7ebdbc84cca
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-04-14 05:47:34 +0700

    Menambahkan memoized component untuk middle

---
+++ src/App/Components/Children/MiddleChildren.js
@@ -0,0 +1,73 @@
+import { memo } from "react";
+import { LoadGif } from "../../Util/Image";
+import { Row, Col } from "react-bootstrap";
+
+const commonComprator = (prevProps, nextProps) =>
+  JSON.stringify(prevProps) === JSON.stringify(nextProps);
+
+const BoxInput = memo(({ fireSearch, searchValue, setSearchValue }) => {
+  return (
+    <div className="box-input" id="input-mobile">
+      <h2>Find Anime</h2>
+      <input
+        type="text"
+        placeholder="Search..."
+        id="search-input"
+        autoComplete="off"
+        onKeyUp={fireSearch}
+        onChange={(e) => setSearchValue(e.target.value)}
+        value={searchValue}
+      />
+    </div>
+  );
+}, commonComprator);
+
+const Content = memo(
+  ({ loading, data, fire }) => (
+    <Row className="content" id="middle-content">
+      {loading && (
+        <Col className="col-12 text-center mt-5">
+          <img src={LoadGif} alt="Load" />
+        </Col>
+      )}
+      {!loading &&
+        data.length > 0 &&
+        data.map((ani) => (
+          <Col md={3} className="col-6 mb-4" key={ani.mal_id}>
+            <div
+              className="img-poster"
+              style={{
+                backgroundImage: `url(${ani.image_url.replace("jpg", "webp")})`,
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+              }}
+              onClick={() => fire(ani.mal_id)}
+            />
+            <h3>{ani.title}</h3>
+          </Col>
+        ))}
+    </Row>
+  ),
+  commonComprator
+);
+
+const Season = memo(
+  ({ SeasonData, setCurrent, current }) => (
+    <Row className="season">
+      {SeasonData.map((dat, i) => (
+        <span
+          className={`link-season ${
+            current === dat ? "link-season-active" : ""
+          }`}
+          key={i}
+          onClick={() => setCurrent(dat)}
+        >
+          {dat}
+        </span>
+      ))}
+    </Row>
+  ),
+  (prevProps, nextProps) => prevProps.current === nextProps.current
+);
+
+export { BoxInput, Content, Season };
--- src/App/Components/Middle.js
+++ src/App/Components/Middle.js
@@ -1,11 +1,10 @@
 import { useMemo, useContext } from "react";
-import { Row, Col } from "react-bootstrap";
 
-import { LoadGif } from "../Util/Image";
 import { SearchContext } from "../Context/SearchContext";
 import { CurrentContext } from "../Context/CurrentContext";
 import { DataContext } from "../Context/DataContext";
 import { DetailContext } from "../Context/DetailContext";
+import { BoxInput, Content, Season } from "./Children/MiddleChildren";
 
 function Middle() {
   const {
@@ -21,32 +20,17 @@ function Middle() {
     <section className="web-middle">
       <h2 id="title-season">Anime Season {time.getFullYear()}</h2>
 
-      <Row className="season">
-        {SeasonData.map((dat, i) => (
-          <span
-            className={`link-season ${
-              current === dat ? "link-season-active" : ""
-            }`}
-            key={i}
-            onClick={() => setCurrent(dat)}
-          >
-            {dat}
-          </span>
-        ))}
-      </Row>
+      <Season
+        SeasonData={SeasonData}
+        setCurrent={setCurrent}
+        current={current}
+      />
 
-      <div className="box-input" id="input-mobile">
-        <h2>Find Anime</h2>
-        <input
-          type="text"
-          placeholder="Search..."
-          id="search-input"
-          autoComplete="off"
-          onKeyUp={fireSearch}
-          onChange={(e) => setSearchValue(e.target.value)}
-          value={searchValue}
-        />
-      </div>
+      <BoxInput
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+        fireSearch={fireSearch}
+      />
 
       <div className="result">
         <p>Result of</p>
@@ -55,32 +39,7 @@ function Middle() {
         </h1>
       </div>
 
-      <Row className="content" id="middle-content">
-        {loading && (
-          <Col className="col-12 text-center mt-5">
-            <img src={LoadGif} alt="Load" />
-          </Col>
-        )}
-        {!loading &&
-          data.length > 0 &&
-          data.map((ani) => (
-            <Col md={3} className="col-6 mb-4" key={ani.mal_id}>
-              <div
-                className="img-poster"
-                style={{
-                  backgroundImage: `url(${ani.image_url.replace(
-                    "jpg",
-                    "webp"
-                  )})`,
-                  backgroundPosition: "center",
-                  backgroundSize: "cover",
-                }}
-                onClick={() => fire(ani.mal_id)}
-              />
-              <h3>{ani.title}</h3>
-            </Col>
-          ))}
-      </Row>
+      <Content data={data} loading={loading} fire={fire} />
     </section>
   );
 }
